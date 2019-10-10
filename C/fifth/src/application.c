ad1bf3fa7b41dbd5351e121a11420704dea1a0e8
deppy <deppy@deppyThinkPad.deppy>
2019-10-10 22:34:11 +0200

    Created a 3D enviorment and set up simple movement (and lacking camera)

--- C/fifth/src/application.c
+++ C/fifth/src/application.c
@@ -13,8 +13,8 @@
 
 #define W_WIDTH 960
 #define W_HEIGHT 540
+#define SCREEN_RATIO ((float)((float)W_WIDTH/(float)W_HEIGHT))
 #define FOV 100.0f
-#define ASPECT_RATIO W_WIDTH / W_HEIGHT
 #define FARVAL 900.0f
 #define NEARVAL 10.0f
 
@@ -87,10 +87,10 @@ int main(){
 	printf("%s\n", glGetString(GL_VERSION)); //print GL version
 
 	float positions[] = { //the vertecies for the square we want to render, as well as texture cordinates
-		000.0f, 000.0f, 0.0f, 0.0f,
-		100.0f, 000.0f, 1.0f, 0.0f,
-		100.0f, 100.0f, 1.0f, 1.0f,
-		000.0f, 100.0f, 0.0f, 1.0f
+		000.0f, 000.0f, 0.0f, 0.0f, 0.0f,
+		100.0f, 000.0f, 0.0f, 1.0f, 0.0f,
+		100.0f, 100.0f, 0.0f, 1.0f, 1.0f,
+		000.0f, 100.0f, 0.0f, 0.0f, 1.0f
 	};
 
 	unsigned int indicies[] = { //define positions to use for rendering two triangles to make a square
@@ -105,12 +105,12 @@ int main(){
 	initVertexArray(&vao);
 
 	struct VertexBuffer vb;
-	initVertexBuffer(&vb, positions, sizeof(float) * 4 * 4);
+	initVertexBuffer(&vb, positions, sizeof(float) * 4 * 5);
 
 	struct VertexBufferLayout vbl;
 	initVertexBufferLayout(&vbl);
 
-	pushVertexBufferLayout(&vbl, GL_FLOAT, 2, GL_FALSE);
+	pushVertexBufferLayout(&vbl, GL_FLOAT, 3, GL_FALSE);
 	pushVertexBufferLayout(&vbl, GL_FLOAT, 2, GL_FALSE);
 
 	vertexArrayAddBuffer(&vao, &vb, &vbl);
@@ -122,15 +122,18 @@ int main(){
 	
 	//create shader from source
 	
-	vec3 translationA  = {0, 0, 0};
-	vec3 translationB  = {200, 200, 0};
-	
+	vec3 translationA  = {00, 00, -200};
+	vec3 translationB  = {30, 00, -150};
+
+	vec3 camera = {0, 0, 0};
+	vec3 negCam = {0, 0, 0};
+	float xrot = 0.0f;
+	float yrot = 0.0f;
+
 	mat4 proj;
-	glm_ortho(0.0f, W_WIDTH, 0.0f, W_HEIGHT, -1.0f, 1.0f, proj);
+    glm_perspective(90.0f, SCREEN_RATIO, 20.00f, 1000.0f, proj);
 	mat4 view;
-	glm_translate_to((mat4)GLM_MAT4_IDENTITY_INIT, (vec3){0, 0, 0}, view);
 	mat4 model;
-	glm_translate_to((mat4)GLM_MAT4_IDENTITY_INIT, (vec3){200, 200, 0}, model);
 
 	mat4 mvp;
 	glm_mat4_mulN((mat4 *[]){&proj, &view, &model}, 3, mvp);
@@ -159,7 +162,42 @@ int main(){
 	while(!glfwWindowShouldClose(window) && glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS){ //loop for program
 		rendererClear();
 
+		//wasd
+		if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS){
+			glm_vec3_sub(camera, (vec3){0.0f, 0.0f, 1.0f}, camera);
+		}
+		if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS){
+			glm_vec3_sub(camera, (vec3){0.0f, 0.0f, -1.0f}, camera);
+		}
+		if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS){
+			glm_vec3_sub(camera, (vec3){-1.0f, 0.0f, 0.0f}, camera);
+		} 
+		if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS){
+			glm_vec3_sub(camera, (vec3){1.0f, 0.0f, 0.0f}, camera);
+		} 
+		glm_vec3_negate_to(camera, negCam);
+		glm_translate_to((mat4)GLM_MAT4_IDENTITY_INIT, negCam, view);
+
+
+		//arrow camera
+		if(glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS){
+			yrot -= 0.5f;
+		}
+		if(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS){
+			yrot += 0.5f;
+		}
+		if(glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS){
+			xrot += 0.5f;
+		} 
+		if(glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS){
+			xrot -= 0.5f;
+		} 
+		glm_rotate_at(view, camera, glm_rad(yrot), (vec3){1.0f, 0.0f, 0.0f});
+		glm_rotate_at(view, camera, glm_rad(xrot), (vec3){0.0f, 1.0f, 0.0f});
+
+
 		glm_translate_to((mat4)GLM_MAT4_IDENTITY_INIT, translationA, model);
+
 		glm_mat4_mulN((mat4 *[]){&proj, &view, &model}, 3, mvp);
 		shaderSetUniformMat4f(&shader, "u_MVP", mvp);
 		rendererDraw(vao, ib, shader);
