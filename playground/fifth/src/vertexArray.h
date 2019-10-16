a60caae52f9443edbe406011aa5f77de5a280f44
deppy <deppy@deppyPC.deppyPC>
2019-10-16 20:43:46 +0200

    Started work on instanced draw calls

--- playground/fifth/src/vertexArray.h
+++ playground/fifth/src/vertexArray.h
@@ -8,6 +8,11 @@ struct VertexArray{
 	unsigned int m_RendererID;
 };
 
+struct InstancedVertexArray{
+	unsigned int m_RendererID;
+	unsigned int instances;
+};
+
 void initVertexArray(struct VertexArray *vertexarray);
 
 void deleteVertexArray(struct VertexArray *vertexarray);
@@ -17,3 +22,15 @@ const void bindVertexArray(struct VertexArray vertexarray);
 const void unbindVertexArray(struct VertexArray vertexarray);
 
 void vertexArrayAddBuffer(const struct VertexArray *vertexarray,const struct VertexBuffer *vertexbuffer, struct VertexBufferLayout *vertexbufferlayout);
+
+//instanced funcitons
+
+void initInstancedVertexArray(struct InstancedVertexArray *vertexarray);
+
+void deleteInstancedVertexArray(struct InstancedVertexArray *vertexarray);
+
+const void bindInstancedVertexArray(struct InstancedVertexArray vertexarray);
+
+const void unbindInstancedVertexArray(struct InstancedVertexArray vertexarray);
+
+void instancedVertexArrayAddBuffer(const struct InstancedVertexArray *vertexarray,const struct VertexBuffer *vertexbuffer, struct VertexBufferLayout *vertexbufferlayout);
